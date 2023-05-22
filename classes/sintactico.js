2d3b6e42ed39f0d717507ccce2e26a73841b0408
guzhdz <89165084+guzhdz@users.noreply.github.com>
2023-05-22 13:42:01 -0600

    Update sintactico.js

--- classes/sintactico.js
+++ classes/sintactico.js
@@ -27,8 +27,8 @@ class Sintactico {
                 this.lonReglas.push(regla[1]);
                 this.simReglas.push(regla[2]);
             }
-            
-            let numFila = parseInt(lines[numReglas].split("").shift());
+            console.log(lines[numReglas]);
+            let numFila = parseInt(lines[numReglas].split("\t").shift());
             numReglas++;
             for (let i = numReglas; i < numReglas + numFila; i++) {
                 lines[i] = lines[i].replace("\r", "");
